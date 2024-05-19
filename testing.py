79731d90aa85ae6bb5ac6a404f47c337fef165e4
Ariza971 <232410103035@mail.unej.ac.id>
2024-05-19 21:59:25 +0700

    nyoba

--- testing.py
+++ testing.py
@@ -1,8 +1,12 @@
 from tkinter import *
 from tkinter import ttk
 
+# def click() :
+#     print("Topik Kontol")
 def click() :
-    print("Topik Kontol")
+    output_text.insert(INSERT, "Success\n")
+
+# print("output_text : ") 
 
 window = Tk()
 window.geometry()
@@ -16,5 +20,9 @@ frame = ttk.Frame(window, width=500, height=500, padding="10").grid(column=0, ro
 
 button = ttk.Button(frame, text="CLICK", command=click).grid(column=0, row=0, sticky=(N, E))
 # button.pack()
+
+#Output Window test
+output_text = Text(frame, width=10, height=10)
+output_text.grid(column=1, row=0, sticky=(N, E, W, S))
 
 window.mainloop()
