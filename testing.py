013fd57e85d428d80839729e6319c798a1a5bb9d
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2024-05-25 21:00:39 +0700

    test

--- testing.py
+++ testing.py
@@ -1,24 +1,36 @@
 from tkinter import *
 from tkinter import ttk
 
-def click() :
-    output_text.insert(INSERT, "Success\n")
+# def click() :
+#     output_text.insert(INSERT, "Success\n")
 
-window = Tk()
-window.geometry()
-window.title('Page 1')
+# window = Tk()
+# window.geometry()
+# window.title('Page 1')
 
-info = ttk.Style()
-info.configure('Info.TFrame', background='blue', borderWidth=5)
+# info = ttk.Style()
+# info.configure('Info.TFrame', background='blue', borderWidth=5)
 
-frame = ttk.Frame(window, width=500, height=500, padding="10").grid(column=0, row=0, sticky=(N,W,E,S))
+# frame = ttk.Frame(window, width=500, height=500, padding="10").grid(column=0, row=0, sticky=(N,W,E,S))
 
 
-button = ttk.Button(frame, text="CLICK", command=click).grid(column=0, row=0, sticky=(N, E))
-# button.pack()
+# button = ttk.Button(frame, text="CLICK", command=click).grid(column=0, row=0, sticky=(N, E))
+# # button.pack()
 
-#Output Window test
-output_text = Text(frame, width=10, height=10)
-output_text.grid(column=1, row=0, sticky=(N, E, W, S))
+# #Output Window test
+# output_text = Text(frame, width=10, height=10)
+# output_text.grid(column=1, row=0, sticky=(N, E, W, S))
 
-window.mainloop()
+app = Tk()
+app.geometry('720x420')
+app.title("Login")
+
+# inputFrame = ttk.Frame(app, padding=10)
+# inputFrame.pack(padx=10, pady=10)
+inputFrame = Frame(app, background='Green', padx=10, pady=10)
+inputFrame.pack(padx=10, pady=10)
+
+submit = ttk.Button(inputFrame, text="Submit")
+submit.pack()
+
+app.mainloop()
