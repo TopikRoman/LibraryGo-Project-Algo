a79c36212e5000906de937ab0fff655b086445cb
Ariza971 <232410103035@mail.unej.ac.id>
2024-05-25 19:51:01 +0700

    coba sort

---
+++ coba.py
@@ -0,0 +1,84 @@
+import psycopg2
+
+def fetch_books():
+    conn = psycopg2.connect(
+        dbname="LibraryGo",
+        user="postgres",
+        password="12345678",
+        host="localhost",
+        port="5432"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT id_buku, judul_buku, id_genre FROM buku")
+    #id_buku[0], judul_buku[1], id_genre[2]
+    buku = cur.fetchall()
+    cur.close()
+    conn.close()
+    return buku
+
+def merge_sort(buku):
+    if len(buku) > 1:
+        mid = len(buku) // 2
+        left_half = buku[:mid]
+        right_half = buku[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        
+        while i < len(left_half) and j < len(right_half):
+            #left_half[i][2] dan right_half[j][2] merujuk ke id_genre dari buku.
+            #left_half[i][1] dan right_half[j][1] merujuk ke judul_buku dari buku.
+            if left_half[i][2] < right_half[j][2] or (left_half[i][2] == right_half[j][2] and left_half[i][1] < right_half[j][1]):
+
+                buku[k] = left_half[i]
+                i += 1
+
+            else:
+                buku[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            buku[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            buku[k] = right_half[j]
+            j += 1
+            k += 1
+
+def print_books(buku):
+    for book in buku:
+        print(book)
+
+if __name__ == "__main__":
+    buku = fetch_books()
+    print("Buku sebelum sorting:")
+    print_books(buku)
+    
+    merge_sort(buku)
+    
+    print("\Buku setelah sorting:")
+    print_books(buku)
+
+
+#INSERT TO DATABASE TABLE BUKU
+# INSERT INTO genre (id_genre, nama_genre) VALUES
+# (1, 'Fiction'), (2, 'Non-Fiction'), (3, 'Science'), (4, 'History'), (5, 'Biography');
+
+# -- insert 100 buku into the buku table
+# DO $$
+# BEGIN
+#     FOR i IN 1..80 LOOP
+#         INSERT INTO buku (id_buku, judul_buku, tahun_terbit, penerbit, id_genre)
+#         VALUES (i, 'Book ' || i, '2000-01-01', 'Publisher ' || i, i % 5 + 1);
+#     END LOOP;
+#     FOR i IN 81..100 LOOP
+#         INSERT INTO buku (id_buku, judul_buku, tahun_terbit, penerbit, id_genre)
+#         VALUES (i, 'Book ' || i, '2000-01-01', 'Publisher ' || i, 1); -- Genre 1 for 20 books
+#     END LOOP;
+# END $$;
