269504ce305dc79dfc933ae4695ac67de66ce5d4
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2024-05-26 23:52:04 +0700

    update

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ if __name__ == "__main__" :
         targetMenu = dashboard.dashboard()
         print(targetMenu)
 
-        match targetMenu[0] :
+        match targetMenu :
             case 'Data Peminjaman' :
                 tambahBuku.tambahBuku()
             case _ :
