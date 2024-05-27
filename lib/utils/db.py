53df6df66af8de20f457d18f1052c94755dea7a7
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2024-05-27 12:54:38 +0700

    update

--- lib/utils/db.py
+++ lib/utils/db.py
@@ -19,3 +19,24 @@ def menambahkanData(namaTabel, namaKolom, values):
     cur.execute("INSERT INTO " + namaTabel + " " + f'({namaKolom})' + " VALUES " + f'{values}'.replace("[", "(").replace("]", ")"))
     conn.commit()
     return
+
+
+def loginQuery(username, password) :
+    table = ''
+    primaryKey = ''
+
+    if len(password) == 8 :
+        table = 'pustakawan'
+        primaryKey = 'nip'
+    elif len(password) == 6 :
+        table = 'anggota_perpustakaan'
+        primaryKey = 'nip'
+
+    cur.execute(f"SELECT * FROM {table} WHERE email = '{username}' AND {primaryKey} = '{password}'")
+    
+    data = cur.fetchall()
+
+    if len(data) == 0 :
+        return 'Akun tidak ditemukan'
+    
+    return data
