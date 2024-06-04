030a627f690f971496c56e693a5697b555648ed5
Eggy Tio <eggytiowandana1@gmail.com>
2024-06-04 20:52:20 +0700

    Edit data Pustakawan

--- lib/utils/db.py
+++ lib/utils/db.py
@@ -2,9 +2,9 @@ from pickle import NONE
 import psycopg2
 
 conn = psycopg2.connect(
-    dbname="LibraryGo.Baru",
+    dbname="LibraryGo",
     user="postgres",
-    password="19Januari",
+    password="Easyjust123",
     host="localhost",
     port="5432"
 )
